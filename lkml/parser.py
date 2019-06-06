810d04458c69d8c3c321776cbe08c9ed5df63e64
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2019-06-06 13:03:10 -0400

    Implement basic Parsing class that uses parismonious.

--- lkml/parser.py
+++ lkml/parser.py
@@ -2,4 +2,24 @@ from parsimonious.grammar import Grammar
 
 
 def load(filename):
-    pass
+    with open(filename, "r") as file:
+        lookml = file.read()
+
+    parser = Parser(lookml)
+    return parser.parse()
+
+
+class Parser(object):
+    grammar = Grammar(
+        """
+        block = block_type ":" block_name "{" block* "}"
+        block_type = "model" / "view"
+        block_name = ~r"[a-z_]+"
+    """
+    )
+
+    def __init__(self, text):
+        self.text = text
+
+    def parse(self):
+        return self.grammar.parse(self.text)
