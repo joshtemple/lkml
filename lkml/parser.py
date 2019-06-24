184406b2e53aba068c2e4fb50b05defa758a1d05
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2019-06-24 18:42:02 -0400

    Raise an error if about to overwrite a key.

--- lkml/parser.py
+++ lkml/parser.py
@@ -97,6 +97,11 @@ class Parser:
                 target[plural_key].append(update)
             else:
                 target[plural_key] = [update]
+        elif key in target.keys():
+            raise KeyError(
+                f"Key {key} already exists in tree "
+                "and would overwrite the existing value."
+            )
         else:
             target[key] = update
 
