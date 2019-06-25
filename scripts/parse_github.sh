29a9a698f31b72250aff7e59ac7028dc0cd6de8e
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2019-06-25 16:00:03 -0400

    Move errored files into a separate folder for later inspection.

--- scripts/parse_github.sh
+++ scripts/parse_github.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
+mkdir github/errored
+
 for file in github/*
 do
   echo "$file"
-  lkml "$file" > /dev/null
+  if ! lkml "$file" > /dev/null; then
+    cp "$file" github/errored
+  fi
 done
