9c6f50dc41d0440a80afb268b9245cee8dc5f767
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2019-06-25 20:12:03 -0400

    Add speed benchmarking code.

---
+++ scripts/benchmark.py
@@ -0,0 +1,27 @@
+import time
+from pathlib import Path
+import lkml
+from statistics import stdev, mean
+
+times = []
+
+directory = Path(__file__).parent.parent.resolve() / "github"
+for i, path in enumerate(directory.glob("*.lkml")):
+    with path.open("r") as file:
+        try:
+            start = time.process_time()
+            lkml.load(file)
+            end = time.process_time()
+        except SyntaxError:
+            continue
+        else:
+            execution_time = end - start
+            times.append(execution_time)
+
+avg_time = mean(times)
+standard_deviation = stdev(times)
+
+print(
+    f"Average process execution time: {avg_time * 100:.1f} "
+    f"+/- {standard_deviation * 100:.1f} ms"
+)
