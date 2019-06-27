412b86b13996a8e726b496ff16f2e82186a9b41f
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2019-06-27 14:47:36 -0400

    Test the rest of the CLI function.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,6 @@
+import io
 from pathlib import Path
+from unittest.mock import patch
 import logging
 import pytest
 import lkml
@@ -20,3 +22,12 @@ def test_debug_flag_is_parsed_to_log_level_debug(lookml_path):
 def test_absence_of_debug_flag_is_parsed_to_log_level_warn(lookml_path):
     args = lkml.parse_args([lookml_path])
     assert args.log_level == logging.WARN
+
+
+@patch("lkml.load")
+@patch("lkml.parse_args")
+def test_run_cli(mock_parse_args, mock_load, lookml_path):
+    mock_parse_args.return_value.file = io.StringIO()
+    mock_parse_args.return_value.log_level = logging.WARN
+    mock_load.return_value = {"a": 1}
+    lkml.cli()
