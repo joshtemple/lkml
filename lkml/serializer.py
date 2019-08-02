1693ff06ba0e8cee9f1336397941747be28a5cc7
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2019-08-02 11:55:10 -0400

    Add top-level serialize function.

--- lkml/serializer.py
+++ lkml/serializer.py
@@ -21,6 +21,10 @@ class Serializer:
         self.indent = self.base_indent * self.indent_level
         self.newline_indent = "\n" + self.indent
 
+    def serialize(self, obj: Dict):
+        for key, value in obj.items():
+            yield from self.write_any(key, value)
+
     def expand_list(self, key: str, values: Iterable):
         stripped_key = key.rstrip("s")
         for i, value in enumerate(values):
@@ -97,68 +101,3 @@ class Serializer:
             yield " ;;"
         else:
             yield value
-
-    # def serialize_string(self, key: str, obj: str):
-    #     if key in QUOTED_LITERAL_KEYS:
-    #         yield '"'
-    #         yield obj
-    #         yield '"'
-    #     elif key in EXPR_BLOCK_KEYS:
-    #         yield obj
-    #         yield " ;;"
-    #     else:
-    #         yield obj
-    #
-    # def serialize_dict(self, key, obj):
-    #     try:
-    #         name = obj.pop("name")
-    #     except KeyError as error:
-    #         yield "{"
-    #     else:
-    #         yield f"{name} " + "{"
-    #
-    #     if obj:
-    #         yield "\n"
-    #         self.increase_indent_level()
-    #         for key, value in obj.items():
-    #             yield from self.serialize(key, value)
-    #             yield "\n"
-    #         self.decrease_indent_level()
-    #
-    #     yield f"{self.indent}" + "}\n\n"
-    #
-    # def serialize_list(self, key: str, obj: list):
-    #     if all(isinstance(item, str) for item in obj):
-    #         if key is not None:
-    #             yield f"{self.indent}{key}: "
-    #         yield "["
-    #         self.increase_indent_level()
-    #         for i, value in enumerate(obj):
-    #             if i > 0:
-    #                 yield ","
-    #             yield f"{self.newline_indent}"
-    #             yield from self.serialize_string(key, value)
-    #         self.decrease_indent_level()
-    #         yield f"{self.newline_indent}]"
-    #     else:
-    #         for item in obj:
-    #             yield from self.serialize(key=key.rstrip("s"), obj=item)
-    #
-    # def serialize(self, key, obj):
-    #     if isinstance(obj, dict):
-    #         if key is not None:
-    #             yield f"{self.indent}{key}: "
-    #             yield from self.serialize_dict(key, obj)
-    #         else:
-    #             for key, value in obj.items():
-    #                 yield from self.serialize(key, value)
-    #     elif isinstance(obj, (list, tuple)):
-    #         yield from self.serialize_list(key, obj)
-    #     elif isinstance(obj, str):
-    #         if key is not None:
-    #             yield f"{self.indent}{key}: "
-    #         yield from self.serialize_string(key, obj)
-    #     elif isinstance(obj, bool):
-    #         if key is not None:
-    #             yield f"{self.indent}{key}: "
-    #         yield "yes" if obj else "no"
