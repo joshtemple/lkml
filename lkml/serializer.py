417fe5c9a7c38df94b839423e24938e2b373345d
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2019-08-02 10:43:24 -0400

    Add basic functions for writing pairs, keys, and values.

--- lkml/serializer.py
+++ lkml/serializer.py
@@ -20,67 +20,91 @@ class Serializer:
         self.indent = self.base_indent * self.indent_level
         self.newline_indent = "\n" + self.indent
 
-    def serialize_string(self, key: str, obj: str):
+    def write_block(self, key: str, fields: dict, name=None):
+        pass
+
+    def write_set(self, key: str, values: list):
+        pass
+
+    def write_pair(self, key: str, value: str):
+        yield from self.write_key(key)
+        yield from self.write_value(key, value)
+
+    def write_key(self, key: str):
+        yield f"{self.indent}{key}: "
+
+    def write_value(self, key: str, value: str):
         if key in QUOTED_LITERAL_KEYS:
             yield '"'
-            yield obj
+            yield value
             yield '"'
         elif key in EXPR_BLOCK_KEYS:
-            yield obj
+            yield value
             yield " ;;"
         else:
-            yield obj
+            yield value
 
-    def serialize_dict(self, key, obj):
-        try:
-            name = obj.pop("name")
-        except KeyError as error:
-            yield "{"
-        else:
-            yield f"{name} " + "{"
-
-        if obj:
-            yield "\n"
-            self.increase_indent_level()
-            for key, value in obj.items():
-                yield from self.serialize(key, value)
-                yield "\n"
-            self.decrease_indent_level()
-
-        yield f"{self.indent}" + "}\n\n"
-
-    def serialize_list(self, key: str, obj: list):
-        if all(isinstance(item, str) for item in obj):
-            if key is not None:
-                yield f"{self.indent}{key}: "
-            yield "["
-            self.increase_indent_level()
-            for i, value in enumerate(obj):
-                if i > 0:
-                    yield ","
-                yield f"{self.newline_indent}"
-                yield from self.serialize_string(key, value)
-            self.decrease_indent_level()
-            yield f"{self.newline_indent}]"
-        else:
-            for item in obj:
-                yield from self.serialize(key=key.rstrip("s"), obj=item)
-
-    def serialize(self, key, obj):
-        if isinstance(obj, dict):
-            if key is not None:
-                yield f"{self.indent}{key}: "
-                yield from self.serialize_dict(key, obj)
-            else:
-                for key, value in obj.items():
-                    yield from self.serialize(key, value)
-        elif isinstance(obj, (list, tuple)):
-            yield from self.serialize_list(key, obj)
-        elif isinstance(obj, str):
-            if key is not None:
-                yield f"{self.indent}{key}: "
-            yield from self.serialize_string(key, obj)
-        elif isinstance(obj, bool):
-            if key is not None:
-                yield f"{self.indent}{key}: "
-            yield "yes" if obj else "no"
+    # def serialize_string(self, key: str, obj: str):
+    #     if key in QUOTED_LITERAL_KEYS:
+    #         yield '"'
+    #         yield obj
+    #         yield '"'
+    #     elif key in EXPR_BLOCK_KEYS:
+    #         yield obj
+    #         yield " ;;"
+    #     else:
+    #         yield obj
+    #
+    # def serialize_dict(self, key, obj):
+    #     try:
+    #         name = obj.pop("name")
+    #     except KeyError as error:
+    #         yield "{"
+    #     else:
+    #         yield f"{name} " + "{"
+    #
+    #     if obj:
+    #         yield "\n"
+    #         self.increase_indent_level()
+    #         for key, value in obj.items():
+    #             yield from self.serialize(key, value)
+    #             yield "\n"
+    #         self.decrease_indent_level()
+    #
+    #     yield f"{self.indent}" + "}\n\n"
+    #
+    # def serialize_list(self, key: str, obj: list):
+    #     if all(isinstance(item, str) for item in obj):
+    #         if key is not None:
+    #             yield f"{self.indent}{key}: "
+    #         yield "["
+    #         self.increase_indent_level()
+    #         for i, value in enumerate(obj):
+    #             if i > 0:
+    #                 yield ","
+    #             yield f"{self.newline_indent}"
+    #             yield from self.serialize_string(key, value)
+    #         self.decrease_indent_level()
+    #         yield f"{self.newline_indent}]"
+    #     else:
+    #         for item in obj:
+    #             yield from self.serialize(key=key.rstrip("s"), obj=item)
+    #
+    # def serialize(self, key, obj):
+    #     if isinstance(obj, dict):
+    #         if key is not None:
+    #             yield f"{self.indent}{key}: "
+    #             yield from self.serialize_dict(key, obj)
+    #         else:
+    #             for key, value in obj.items():
+    #                 yield from self.serialize(key, value)
+    #     elif isinstance(obj, (list, tuple)):
+    #         yield from self.serialize_list(key, obj)
+    #     elif isinstance(obj, str):
+    #         if key is not None:
+    #             yield f"{self.indent}{key}: "
+    #         yield from self.serialize_string(key, obj)
+    #     elif isinstance(obj, bool):
+    #         if key is not None:
+    #             yield f"{self.indent}{key}: "
+    #         yield "yes" if obj else "no"
--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -7,166 +7,201 @@ def serializer():
     return lkml.Serializer()
 
 
-def test_serialize_dict_with_unquoted_literals(serializer):
-    generator = serializer.serialize_dict(
-        key="bind_fields", obj={"from_field": "field_name", "to_field": "field_name"}
-    )
+def test_write_value_with_unquoted_literal(serializer):
+    generator = serializer.write_value(key="hidden", value="no")
     result = "".join(generator)
     print(result)
-    assert result == "".join(
-        ("{\n", "  from_field: field_name\n", "  to_field: field_name\n", "}")
-    )
+    assert result == "no"
 
 
-def test_serialize_dict_with_quoted_literals(serializer):
-    generator = serializer.serialize_dict(
-        key="dimension",
-        obj={
-            "label": "Dimension Name",
-            "group_label": "Group Name",
-            "description": "A dimension description.",
-        },
-    )
+def test_write_value_with_quoted_literal(serializer):
+    generator = serializer.write_value(key="label", value="Dimension Name")
     result = "".join(generator)
     print(result)
-    assert result == "".join(
-        (
-            "{\n",
-            '  label: "Dimension Name"\n',
-            '  group_label: "Group Name"\n',
-            '  description: "A dimension description."\n',
-            "}",
-        )
-    )
+    assert result == '"Dimension Name"'
 
 
-def test_serialize_dict_with_name(serializer):
-    generator = serializer.serialize_dict(
-        key="dimension", obj={"name": "dimension_name", "label": "Dimension Name"}
-    )
+def test_write_key(serializer):
+    generator = serializer.write_key(key="hidden")
     result = "".join(generator)
     print(result)
-    assert result == "".join(("dimension_name {\n", '  label: "Dimension Name"\n', "}"))
+    assert result == "hidden: "
 
 
-def test_serialize_nested_dict(serializer):
-    generator = serializer.serialize_dict(
-        key="derived_table",
-        obj={
-            "explore_source": {
-                "bind_filters": {"from_field": "field_name", "to_field": "field_name"},
-                "name": "explore_name",
-            }
-        },
-    )
+def test_write_pair_with_unquoted_literal(serializer):
+    generator = serializer.write_pair(key="hidden", value="no")
     result = "".join(generator)
     print(result)
-    assert result == "".join(
-        (
-            "{\n",
-            "  explore_source: explore_name {\n",
-            "    bind_filters: {\n",
-            "      from_field: field_name\n",
-            "      to_field: field_name\n",
-            "    }\n",
-            "  }\n",
-            "}",
-        )
-    )
+    assert result == "hidden: no"
 
 
-def test_serialize_empty_dict_with_name(serializer):
-    generator = serializer.serialize_dict(
-        key="dimension", obj={"name": "dimension_name"}
-    )
+def test_write_pair_with_quoted_literal(serializer):
+    generator = serializer.write_pair(key="label", value="Dimension Name")
     result = "".join(generator)
     print(result)
-    assert result == "dimension_name {}"
+    assert result == 'label: "Dimension Name"'
 
 
-def test_serialize_empty_dict_without_name(serializer):
-    generator = serializer.serialize_dict(key="dimension", obj={})
-    result = "".join(generator)
-    print(result)
-    assert result == "{}"
-
-
-def test_serialize_list_with_unquoted_literals(serializer):
-    generator = serializer.serialize_list(
-        key="fields", obj=["dimension_one", "dimension_two", "dimension_three"]
-    )
-    result = "".join(generator)
-    print(result)
-    assert result == "[\n  dimension_one,\n  dimension_two,\n  dimension_three\n]"
-
-
-def test_serialize_list_with_quoted_literals(serializer):
-    generator = serializer.serialize_list(
-        key="sortkeys", obj=["column_one", "column_two", "column_three"]
-    )
-    result = "".join(generator)
-    print(result)
-    assert result == '[\n  "column_one",\n  "column_two",\n  "column_three"\n]'
-
-
-def test_serialize_with_plural_key(serializer):
-    generator = serializer.serialize(
-        key=None,
-        obj={"dimensions": [{"name": "dimension_one"}, {"name": "dimension_two"}]},
-    )
-    # print(list(generator))
-    result = "".join(generator)
-    print(result)
-    assert result == "dimension: dimension_one {}\n\ndimension: dimension_two {}"
-
-
-def test_serialize_view_with_multiple_dimensions(serializer):
-    generator = serializer.serialize(
-        key=None,
-        obj={
-            "views": [
-                {
-                    "sql_table_name": "schema.table_name",
-                    "dimensions": [
-                        {
-                            "type": "string",
-                            "sql": "${TABLE}.a_dimension",
-                            "name": "a_dimension",
-                        },
-                        {
-                            "type": "number",
-                            "sql": "${TABLE}.another_dimension",
-                            "name": "another_dimension",
-                        },
-                        {
-                            "type": "yesno",
-                            "sql": "${TABLE}.yet_another_dimension",
-                            "name": "yet_another_dimension",
-                        },
-                    ],
-                    "dimension_groups": [
-                        {
-                            "type": "time",
-                            "timeframes": [
-                                "raw",
-                                "time",
-                                "hour_of_day",
-                                "date",
-                                "day_of_week",
-                                "week",
-                                "month",
-                                "quarter",
-                                "year",
-                            ],
-                            "sql": "${TABLE}.a_dimension_group",
-                            "name": "a_dimension_group",
-                        }
-                    ],
-                    "name": "view_name",
-                }
-            ]
-        },
-    )
-    result = "".join(generator)
-    print(result)
-    assert False
+# def test_serialize_dict_with_unquoted_literals(serializer):
+#     generator = serializer.serialize_dict(
+#         key="bind_fields", obj={"from_field": "field_name", "to_field": "field_name"}
+#     )
+#     result = "".join(generator)
+#     print(result)
+#     assert result == "".join(
+#         ("{\n", "  from_field: field_name\n", "  to_field: field_name\n", "}")
+#     )
+#
+#
+# def test_serialize_dict_with_quoted_literals(serializer):
+#     generator = serializer.serialize_dict(
+#         key="dimension",
+#         obj={
+#             "label": "Dimension Name",
+#             "group_label": "Group Name",
+#             "description": "A dimension description.",
+#         },
+#     )
+#     result = "".join(generator)
+#     print(result)
+#     assert result == "".join(
+#         (
+#             "{\n",
+#             '  label: "Dimension Name"\n',
+#             '  group_label: "Group Name"\n',
+#             '  description: "A dimension description."\n',
+#             "}",
+#         )
+#     )
+#
+#
+# def test_serialize_dict_with_name(serializer):
+#     generator = serializer.serialize_dict(
+#         key="dimension", obj={"name": "dimension_name", "label": "Dimension Name"}
+#     )
+#     result = "".join(generator)
+#     print(result)
+#     assert result == "".join(("dimension_name {\n", '  label: "Dimension Name"\n', "}"))
+#
+#
+# def test_serialize_nested_dict(serializer):
+#     generator = serializer.serialize_dict(
+#         key="derived_table",
+#         obj={
+#             "explore_source": {
+#                 "bind_filters": {"from_field": "field_name", "to_field": "field_name"},
+#                 "name": "explore_name",
+#             }
+#         },
+#     )
+#     result = "".join(generator)
+#     print(result)
+#     assert result == "".join(
+#         (
+#             "{\n",
+#             "  explore_source: explore_name {\n",
+#             "    bind_filters: {\n",
+#             "      from_field: field_name\n",
+#             "      to_field: field_name\n",
+#             "    }\n",
+#             "  }\n",
+#             "}",
+#         )
+#     )
+#
+#
+# def test_serialize_empty_dict_with_name(serializer):
+#     generator = serializer.serialize_dict(
+#         key="dimension", obj={"name": "dimension_name"}
+#     )
+#     result = "".join(generator)
+#     print(result)
+#     assert result == "dimension_name {}"
+#
+#
+# def test_serialize_empty_dict_without_name(serializer):
+#     generator = serializer.serialize_dict(key="dimension", obj={})
+#     result = "".join(generator)
+#     print(result)
+#     assert result == "{}"
+#
+#
+# def test_serialize_list_with_unquoted_literals(serializer):
+#     generator = serializer.serialize_list(
+#         key="fields", obj=["dimension_one", "dimension_two", "dimension_three"]
+#     )
+#     result = "".join(generator)
+#     print(result)
+#     assert result == "[\n  dimension_one,\n  dimension_two,\n  dimension_three\n]"
+#
+#
+# def test_serialize_list_with_quoted_literals(serializer):
+#     generator = serializer.serialize_list(
+#         key="sortkeys", obj=["column_one", "column_two", "column_three"]
+#     )
+#     result = "".join(generator)
+#     print(result)
+#     assert result == '[\n  "column_one",\n  "column_two",\n  "column_three"\n]'
+#
+#
+# def test_serialize_with_plural_key(serializer):
+#     generator = serializer.serialize(
+#         key=None,
+#         obj={"dimensions": [{"name": "dimension_one"}, {"name": "dimension_two"}]},
+#     )
+#     # print(list(generator))
+#     result = "".join(generator)
+#     print(result)
+#     assert result == "dimension: dimension_one {}\n\ndimension: dimension_two {}"
+#
+#
+# def test_serialize_view_with_multiple_dimensions(serializer):
+#     generator = serializer.serialize(
+#         key=None,
+#         obj={
+#             "views": [
+#                 {
+#                     "sql_table_name": "schema.table_name",
+#                     "dimensions": [
+#                         {
+#                             "type": "string",
+#                             "sql": "${TABLE}.a_dimension",
+#                             "name": "a_dimension",
+#                         },
+#                         {
+#                             "type": "number",
+#                             "sql": "${TABLE}.another_dimension",
+#                             "name": "another_dimension",
+#                         },
+#                         {
+#                             "type": "yesno",
+#                             "sql": "${TABLE}.yet_another_dimension",
+#                             "name": "yet_another_dimension",
+#                         },
+#                     ],
+#                     "dimension_groups": [
+#                         {
+#                             "type": "time",
+#                             "timeframes": [
+#                                 "raw",
+#                                 "time",
+#                                 "hour_of_day",
+#                                 "date",
+#                                 "day_of_week",
+#                                 "week",
+#                                 "month",
+#                                 "quarter",
+#                                 "year",
+#                             ],
+#                             "sql": "${TABLE}.a_dimension_group",
+#                             "name": "a_dimension_group",
+#                         }
+#                     ],
+#                     "name": "view_name",
+#                 }
+#             ]
+#         },
+#     )
+#     result = "".join(generator)
+#     print(result)
+#     assert False
