891bc0f63feac8e20c9226b584eae20b02399d97
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2019-08-05 18:15:18 -0400

    Modify test script to try and re-parse dumped LookML.

--- scripts/test_github.py
+++ scripts/test_github.py
@@ -8,19 +8,30 @@ logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
     github_path = Path(__file__).resolve().parent.parent / "github"
-    logging.info("Deleting and recreating github/errored")
-    errored_path = github_path / "errored"
-    shutil.rmtree(errored_path)
-    errored_path.mkdir()
+
+    logging.info("Deleting and recreating error directories")
+    for dirname in ["load_errors", "dump_errors"]:
+        path = github_path / dirname
+        if path.exists():
+            shutil.rmtree(path)
+        path.mkdir()
 
     logging.info("Parsing and serializing LookML from...")
     for path in github_path.glob("*.lkml"):
         logging.info(path)
         with path.open("r") as file:
             text = file.read()
+
         try:
             parsed = lkml.load(text)
-            dumped = lkml.dump(parsed)
         except Exception:
-            shutil.copy(path, github_path / "errored" / path.name)
-            logging.exception(f"Error parsing or serializing {path}")
+            shutil.copy(path, github_path / "load_errors" / path.name)
+            logging.exception(f"Error parsing {path}")
+
+        try:
+            dumped = lkml.dump(parsed)
+            re_parsed = lkml.load(dumped)
+        except Exception:
+            with open(github_path / "dump_errors" / path.name, "w+") as file:
+                file.write(dumped)
+            logging.exception(f"Error serializing {path}")
